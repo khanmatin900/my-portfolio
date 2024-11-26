69612023a693512956059da185e64d05afdbf32c
khanmatin900 <matimkhan480@gmail.com>
2024-11-26 22:34:34 +0530

    add dark mode btn features

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const checkbox = document.getElementById("checkbox")
-checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
+// const checkbox = document.getElementById("checkbox")
+// checkbox.addEventListener("change", () => {
+//   document.body.classList.toggle("dark");
+// });
+
+const checkbox = document.getElementById('checkbox');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme === 'dark') {
+    document.body.classList.add('dark');
+}
+
+checkbox.addEventListener('click', function() {
+    document.body.classList.toggle('dark');
+    let theme = 'light';
+    if (document.body.classList.contains('dark')) {
+        theme = 'dark';
+    }
+    localStorage.setItem('theme', theme);
 });
